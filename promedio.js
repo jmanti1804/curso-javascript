50a08d4365b49c4e639212f35573df9a80fd715b
Joselo Mantilla <mantilla.carlosjoselo@gmail.com>
2022-01-14 21:42:06 -0500

    Agregando funcionalidades y hojas de estilo promedio, mediana

--- promedio.js
+++ promedio.js
@@ -1,9 +1,14 @@
-
 function calcularMediaAritmetica(lista){
-    let sumaLista = 0;
-    for (let i = 0; i < lista.length; i ++){
-        sumaLista += lista[i];
-    }
+    //let sumaLista = 0;
+    //for (let i = 0; i < lista.length; i ++){
+    //    sumaLista += lista[i];
+    //}
+
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento){ //Envio funcion anonima sin nombre
+            return valorAcumulado + nuevoElemento;
+        }
+    );
     const promedioLista = sumaLista / lista.length;
     return promedioLista;
 }
